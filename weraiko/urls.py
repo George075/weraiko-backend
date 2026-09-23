d70533bb0776e2fa32a30be5cebbc35815bfff12
George Okoth <okothgeorge075@gmail.com>
2026-09-23 12:08:47 +0300

    Force CORS headers via custom middleware

--- weraiko/urls.py
+++ weraiko/urls.py
@@ -3,6 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+class ForceCorsMiddleware:
+    """Force CORS headers on every response, no matter what."""
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+
+
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('api/', include('jobs.urls')),
