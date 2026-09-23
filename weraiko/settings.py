529646dee691e6cd6687877c279809bcc0f4fdf7
George Okoth <okothgeorge075@gmail.com>
2026-09-23 12:11:59 +0300

    Force CORS headers via custom middleware

--- weraiko/settings.py
+++ weraiko/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'weraiko.urls.ForceCorsMiddleware',
 ]
 
 ROOT_URLCONF = 'weraiko.urls'
